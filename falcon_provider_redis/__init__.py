5fb55b6e74074f829050212bac77903539c88f31
Bracey Summers <bcsummers@gmail.com>
2022-12-16 10:25:30 -0600

    General cleanup - removed metadata file - updated pre-commit config - added codeql github action for security scanning

--- falcon_provider_redis/__init__.py
+++ falcon_provider_redis/__init__.py
@@ -1,12 +1,4 @@
 """Falcon REDIS module."""
 # flake8: noqa
-from .__metadata__ import (
-    __author__,
-    __author_email__,
-    __description__,
-    __license__,
-    __package_name__,
-    __url__,
-    __version__,
-)
-from .utils import RedisClient, redis_client
+# first-party
+from falcon_provider_redis.utils import RedisClient, redis_client
